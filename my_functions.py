495425fa8e9bbdb8d88f583ba3f4d4e1ca9c05ec
heyscottn <heyscottn@gmail.com>
2024-02-07 09:58:48 -0600

    mm

--- my_functions.py
+++ my_functions.py
@@ -3,3 +3,20 @@ from random import randint
 # note the style of import here
 
 
+def draw_grid(screen):
+    pass
+
+def draw_square(screen):
+    pass
+
+def draw_X(screen):
+    pass
+
+def draw_horiz_lines(screen):
+    pass
+
+
+def my_color():
+    pass
+
+
